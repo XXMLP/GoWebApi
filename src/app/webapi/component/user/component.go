cbf0373598506226247e63267e00c2a41099e564
lipeng.min <lipeng.min@dingxiang-inc.com>
2021-05-12 14:20:57 +0800

    添加token解析用户id方法

--- src/app/webapi/component/user/component.go
+++ src/app/webapi/component/user/component.go
@@ -2,6 +2,8 @@ package user
 
 import (
 	"app/webapi/component"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
 )
 
 // New returns a new instance of the endpoint.
@@ -20,8 +22,24 @@ type Endpoint struct {
 func (p *Endpoint) Routes(router component.IRouter) {
 	router.Post("/v1/user", p.Create)
 	router.Get("/v1/user/:user_id", p.Show)
+	router.Get("/v1/userSelf", p.ShowSelf)
 	router.Get("/v1/user", p.Index)
 	router.Put("/v1/user/:user_id", p.Update)
 	router.Delete("/v1/user/:user_id", p.Destroy)
 	router.Delete("/v1/user", p.DestroyAll)
+}
+
+//ParseToken to userId
+func ParseToken(tokenString string) (string, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("jOJtWzbV6qiIuuC60iAnYTbnx7tz8NvxEdEWuaDw31Q="), nil
+	})
+	if claims, ok := token.Claims.(*jwt.StandardClaims); ok {
+		return claims.Audience, nil
+	} else {
+		return "", err
+	}
 }
